dd1e2fa4b05da2d537e53f0a8487d711802345d2
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-07-28 15:14:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 col1, col2, col3 = st.beta_columns([6,1,2])
 with col1:
-  url=st.text_input('Enter working URL',"https://docs.dgl.ai/tutorials/blitz/6_load_data.html")
+  url=st.text_input('Enter working URL',"https://docs.streamlit.io/en/stable/")
   components.iframe(url,height=800,scrolling=True)
 
 
