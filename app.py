cb5728cc40340a098d34d233907b156260529e7d
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-07-28 15:11:15 -0600

    upload app and requirements

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_player import st_player
+import streamlit.components.v1 as components
+st.set_page_config(layout="wide")
+col1, col2, col3 = st.beta_columns([6,1,2])
+with col1:
+  url=st.text_input('Enter working URL',"https://docs.dgl.ai/tutorials/blitz/6_load_data.html")
+  components.iframe(url,height=800,scrolling=True)
+
+
+with col3:
+  url_youtube=st.text_input('Enter youtube URL')
+  # Embed a youtube video
+  st_player(url_youtube)
