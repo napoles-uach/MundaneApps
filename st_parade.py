bcee8491ade47f05aac74604ead3339c8a843885
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-09-02 12:44:54 -0600

    remove

--- st_parade.py
+++ st_parade.py
@@ -8,22 +8,22 @@ st.set_page_config(layout="wide")
 list1=[]
 list2=[]
 maxtags =6 
-keywords = st_tags_sidebar(
-    label='# Enter URLs to work:',
-    text='Press enter to add more',
-    maxtags=maxtags,
-    key="aljnf")
+#keywords = st_tags_sidebar(
+#    label='# Enter URLs to work:',
+#    text='Press enter to add more',
+#    maxtags=maxtags,
+#    key="aljnf")
 
 
-col1, col2, col3 = st.columns([6,1,2])
-with col1:
-  sel_url=st.selectbox('Select url', keywords)
-  components.iframe(sel_url,height=800,scrolling=True)
+#col1, col2, col3 = st.columns([6,1,2])
+#with col1:
+#  sel_url=st.selectbox('Select url', keywords)
+#  components.iframe(sel_url,height=800,scrolling=True)
 
 
-with col3:
-  url_youtube=st.text_input('Enter youtube URL','https://www.youtube.com/watch?v=_daTfgc4u3k')
-  st_player(url_youtube)
+#with col3:
+#  url_youtube=st.text_input('Enter youtube URL','https://www.youtube.com/watch?v=_daTfgc4u3k')
+#  st_player(url_youtube)
 
 
 url = "https://raw.githubusercontent.com/napoles-uach/MundaneApps/main/links.txt"
