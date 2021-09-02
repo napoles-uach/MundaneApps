ce24172946483a04603f1416f89b5801da603a2f
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-09-02 12:51:27 -0600

    cols

--- st_parade.py
+++ st_parade.py
@@ -8,11 +8,7 @@ st.set_page_config(layout="wide")
 list1=[]
 list2=[]
 maxtags =6 
-#keywords = st_tags_sidebar(
-#    label='# Enter URLs to work:',
-#    text='Press enter to add more',
-#    maxtags=maxtags,
-#    key="aljnf")
+
 
 
 #col1, col2, col3 = st.columns([6,1,2])
@@ -34,6 +30,11 @@ for line in file:
 	decoded_line = line.decode("utf-8")
 	urls.append(decoded_line)
 
-#st.write(urls)
-sel_url=st.selectbox('Select url', urls)
-components.iframe(sel_url,height=800,scrolling=True)
+col1, col2, col3 = st.columns([6,1,2])
+with col1:
+  sel_url=st.selectbox('Select url', urls)
+  components.iframe(sel_url,height=800,scrolling=True)
+
+with col3:
+  url_youtube=st.text_input('Enter youtube URL','https://www.youtube.com/watch?v=_daTfgc4u3k')
+  st_player(url_youtube)
