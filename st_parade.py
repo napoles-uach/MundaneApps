04d022dceb80082c9a682693e589895e495265bb
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-10-15 17:44:19 -0600

    page

--- st_parade.py
+++ st_parade.py
@@ -3,7 +3,43 @@ from streamlit_player import st_player
 import streamlit.components.v1 as components
 from streamlit_tags import st_tags, st_tags_sidebar
 import urllib.request
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
+
+BACKGROUND_COLOR = 'white'
+COLOR = 'black'
+
+def set_page_container_style(
+        max_width: int = 2100, max_width_100_percent: bool = False,
+        padding_top: int = 0, padding_right: int = 1, padding_left: int = 1, padding_bottom: int = 10,
+        color: str = COLOR, background_color: str = BACKGROUND_COLOR,
+    ):
+        if max_width_100_percent:
+            max_width_str = f'max-width: 100%;'
+        else:
+            max_width_str = f'max-width: {max_width}px;'
+        st.markdown(
+            f'''
+            <style>
+                .reportview-container .sidebar-content {{
+                    padding-top: {padding_top}rem;
+                }}
+                .reportview-container .main .block-container {{
+                    {max_width_str}
+                    padding-top: {padding_top}rem;
+                    padding-right: {padding_right}rem;
+                    padding-left: {padding_left}rem;
+                    padding-bottom: {padding_bottom}rem;
+                }}
+                .reportview-container .main {{
+                    color: {color};
+                    background-color: {background_color};
+                }}
+            </style>
+            ''',
+            unsafe_allow_html=True,
+        )
+
+set_page_container_style()
 
 hide_st_style = """
             <style>
